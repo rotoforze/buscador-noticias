b377dcad48248149c5456f5b5d30cf910286b5bb
Alex <118481249+rotoforze@users.noreply.github.com>
2025-03-28 08:18:24 +0000

    r-01

--- src/js/idiomaMenu.js
+++ src/js/idiomaMenu.js
@@ -15,14 +15,21 @@ export function cambiarIdioma(idioma) {
 
     switch (idioma) {
         case "es":
-
+            header.innerHTML = "Buscador de noticias📰";
+            idiomas.innerHTML = "Idioma 🌍";
+            btnBuscar.innerHTML = "Buscar";
+            filtroLengua.innerHTML = "Filtrar por país";
+            filtroOrden.innerHTML = "Ordenar por";
+            filtroCantidad.innerHTML = "Cantidad de resultados";
+            fechaDesde.innerHTML = "Desde";
+            fechaHasta.innerHTML = "Hasta";
             break;
 
         case "en":
             header.innerHTML = "News Finder📰";
             idiomas.innerHTML = "Languages 🌍";
             btnBuscar.innerHTML = "Search";
-            filtroLengua.innerHTML = "Country filter";
+            filtroLengua.innerHTML = "Language filter";
             filtroOrden.innerHTML = "Order by";
             filtroCantidad.innerHTML = "News volume";
             fechaDesde.innerHTML = "From";
@@ -30,4 +37,38 @@ export function cambiarIdioma(idioma) {
             break;
     }
 
+    cambiarListaPaises(idioma);
+    cambiarListaOrdenar(idioma);
+}
+
+function cambiarListaPaises(idioma) {
+    const traduccion = new Map([
+        ["es", ["----", "Español", "Alemán", "Inglés", "Frances", "Italiano", "Neerlandes", "Noruego", "Portuges", "Ruso", "Sueco", "Árabe", "Chino"]],
+        ["en", ["----", "Spanish", "German", "English", "French", "Italian", "Dutch", "Norwegian", "Portuguese", "Russian", "Swedish", "Arabic", "Chinese"]]
+    ]);
+    const listaPaises = document.querySelector(".lista-paises");
+
+    const arrayTraduccion = Array.from(traduccion.get(idioma));
+
+    for (const i in listaPaises.children) {
+        if (i < listaPaises.children.length) {
+            listaPaises.children[i].innerHTML = arrayTraduccion[i];
+        }
+    }
+}
+
+function cambiarListaOrdenar(idioma) {
+    const traduccion = new Map([
+        ["es", ["Relevancia", "Popularidad", "Recientes"]],
+        ["en", ["Relevancy", "Popularity", "Newer"]]
+    ]);
+    const ordenar = document.querySelector(".orden");
+
+    const arrayTraduccion = Array.from(traduccion.get(idioma));
+
+    for (const i in ordenar.children) {
+        if (i < ordenar.children.length) {
+            ordenar.children[i].innerHTML = arrayTraduccion[i];
+        }
+    }
 }
